2ea1aee856f5e1cc8c3584bee87ab72b39c9ab28
rbburkey <robinsonbburkey@gmail.com>
2016-02-02 17:26:44 -0500

    Sample calculator

---
+++ tipcalc.rb
@@ -0,0 +1,39 @@
+
+def tipper (num)
+    num / 100
+end
+
+put_a_line
+    puts "\n"
+    puts "​*" *​ 50
+    puts "\n"
+end
+
+puts "How much was your restaurant bill?" 
+bill = gets.chomp.to_f
+puts "Your bill is #{bill}."
+
+puts "Would you like to tip 15%, 18%, 20%?"
+tip_perc = gets.chomp.to_f
+
+tipfinal = tipper(tip_perc)
+
+puts "How many ways are you splitting the bill?"
+splitnumber = gets.chomp.to_f
+
+
+
+taxamt = (0.06)
+
+tax = bill * taxamt
+
+tip = bill * tipfinal
+
+total = bill + tip + tax  
+
+my_share = (total / splitnumber).round(2)
+
+put_a_line
+
+puts "The total output of the bill is $#{total}"
+puts "Your share of the bill is $#{my_share}"
